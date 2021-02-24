93f28e720c84e668195637a4634eba6d62f61b84
onc157 <onc157@yandex.ru>
2021-02-25 02:13:51 +0300

    feat: add node-sass / material-ui feat: filled header feat: add base css modules

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import './style.scss'
 
 const Header = (): JSX.Element => {
-  return <>Header</>
+  return (
+    <div className='header-wrapper'>
+      <div className='header__logo'>2048</div>
+      <div className='header__author'>by ONC</div>
+    </div>
+  )
 }
 
 export default Header
