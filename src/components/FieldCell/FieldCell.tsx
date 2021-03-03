faffec6dc9c2fc7de108b0a280fa13d60a5b9214
onc157 <onc157@yandex.ru>
2021-03-04 01:22:40 +0300

    feat: implemented different color for cells

--- src/components/FieldCell/FieldCell.tsx
+++ src/components/FieldCell/FieldCell.tsx
@@ -7,7 +7,7 @@ type PropTypes = {
 
 const FieldCell: React.FC<PropTypes> = ({ value }): JSX.Element => {
   return (
-    <div className={value ? 'cell-wrapper cell__active' : 'cell-wrapper'}>
+    <div className={value ? `cell-wrapper cell__active cell--${value}` : 'cell-wrapper'}>
       <span className="cell__number">{value}</span>
     </div>
   )
